e85c553b12b232c40a6967abe84272743f296de8
Philip Ni <philipni122@gmail.com>
2017-02-21 09:56:01 -0500

    add support for negative numbers

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,11 +1,17 @@
-class Fibonacci {
+class Main {
   public static int fibonacci(int num) {
     if(num == 0) return 0;
     if(num == 1) return 1;
-    return fibonacci(num - 1) + fibonacci(num - 2);
+    if(num == -1) return -1;
+    
+    if(num > 0)
+      return fibonacci(num - 1) + fibonacci(num - 2);
+    else
+      return fibonacci(num + 1) + fibonacci(num + 2);
   }
   
   public static void main(String[] args) {
-    System.out.println(fibonacci(10));
+    System.out.println(fibonacci(-3));
+    System.out.println(fibonacci(3));
   }
 }
